26dc4cfec5b324f9e781c2f1fb707b3e9c9bf2c4
FREDRICK-01 <fredrickgachuru73@gmail.com>
2021-09-20 00:21:04 +0300

    adds html flders to the template module

---
+++ app/models.py
@@ -0,0 +1,25 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+    def __init__(self,id,name,url,description,category, country):
+        self.id =id
+        self.name= name
+        self.url = url
+        self.description = description
+        self.category = category
+        self.country = country
+class Articles:
+    get_articles = []
+    def __init__(self,title,author,url,description, urlToImage, publishedAt, content):
+        self.title = title
+        self.author = author
+        self.url = url
+        self.description= description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+
+
+
+
